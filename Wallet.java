292f2beb86a37fff75c907b717610844951e28c7
DANIEL ODHIAMBO <danodundo@gmail.com>
2015-10-01 12:22:45 +0300

    first commit

---
+++ Wallet.java
@@ -0,0 +1,23 @@
+package com.wayawaya;
+
+import java.util.Vector;
+
+/**
+ * Created by DODHIAMBO on 9/30/2015.
+ */
+public class Wallet {
+    int id;
+
+    int customer_id;
+
+    Vector<Account> acccounts;
+
+
+
+    void setMoney(Account recipient, Double amount){
+
+    }
+    void widthDraw(Account myaccount, Double amount){
+
+    }
+}
